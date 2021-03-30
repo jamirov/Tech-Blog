0f0c2796209cefc01c53589281b77ef406a2a286
Jasur Amirov <ajasur@gmail.com>
2021-03-29 21:10:26 -0700

    added models handlebars and some  routes

---
+++ controllers/index.js
@@ -0,0 +1,49 @@
+const router = require('express').Router();
+const { Post, User } = require('../models');
+// const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    // const projectData = await Post.findAll({
+    //   include: [
+    //     {
+    //       model: User,
+    //       attributes: ['name'],
+    //     },
+    //   ],
+    // });
+
+    // Serialize data so the template can read it
+    // const projects = projectData.map((project) => project.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+router.get('/signup', async (req, res) => {
+    try {
+      // Get all projects and JOIN with user data
+      // const projectData = await Post.findAll({
+      //   include: [
+      //     {
+      //       model: User,
+      //       attributes: ['name'],
+      //     },
+      //   ],
+      // });
+  
+      // Serialize data so the template can read it
+      // const projects = projectData.map((project) => project.get({ plain: true }));
+  
+      // Pass serialized data and session flag into template
+      res.render('signup');
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
+module.exports = router;
